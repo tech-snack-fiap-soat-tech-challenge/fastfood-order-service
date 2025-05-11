f5a065f3adac29c00e4f6209f2d59fe2c322a943
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-05-11 17:31:25 -0300

    feat: add name and price properties to OrderProductInput and OrderProductOutput with Swagger documentation

--- src/order/core/application/dtos/order-products.input.ts
+++ src/order/core/application/dtos/order-products.input.ts
@@ -1,4 +1,6 @@
 export interface OrderProductInput {
   id: number;
+  name: string;
   quantity: number;
+  price: number;
 }
--- src/order/core/application/dtos/order-products.output.ts
+++ src/order/core/application/dtos/order-products.output.ts
@@ -8,8 +8,20 @@ export class OrderProductOutput {
   id: number;
 
   @ApiProperty({
+    description: 'Name of the product',
+    example: 'Product Name',
+  })
+  name: string;
+
+  @ApiProperty({
     description: 'Quantity of the product in the order',
     example: 1,
   })
   quantity: number;
+
+  @ApiProperty({
+    description: 'Price of the product',
+    example: 100.0,
+  })
+  price: number;
 }
