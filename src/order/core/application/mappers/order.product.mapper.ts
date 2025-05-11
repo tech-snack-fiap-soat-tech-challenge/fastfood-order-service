a9594a3106ee496fd831fd0a03eba9a6e3aec68b
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-05-11 17:39:32 -0300

    feat: enhance OrderProductsMapper to include name and price in toDto and toEntity methods

--- src/order/core/application/mappers/order.product.mapper.ts
+++ src/order/core/application/mappers/order.product.mapper.ts
@@ -1,17 +1,20 @@
 import { OrderProductEntity } from '@order/core/domain/entities/order.product.entity';
 import { OrderProductOutput } from '../dtos/order-products.output';
+import { OrderProductInput } from '../dtos/order-products.input';
 
 export class OrderProductsMapper {
   /* domínio → DTO */
   static toDto(product: OrderProductEntity): OrderProductOutput {
     return {
       id: product.id,
+      name: product.name,
       quantity: product.quantity,
+      price: product.price,
     };
   }
 
   /* DTO (input) → domínio */
-  static toEntity(dto: { id: number; quantity: number }): OrderProductEntity {
-    return new OrderProductEntity(dto.id, dto.quantity);
+  static toEntity(dto: OrderProductInput): OrderProductEntity {
+    return new OrderProductEntity(dto.id, dto.name, dto.quantity, dto.price);
   }
 }
