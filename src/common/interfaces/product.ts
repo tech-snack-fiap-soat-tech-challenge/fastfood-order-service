95e90b959cbdff75218e551b450bf4f26a4478e7
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-05-11 17:30:16 -0300

    feat: add IProduct interface and mock product data

---
+++ src/common/interfaces/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  unit_price: number;
+  category_id: number;
+  stock_quantity: number;
+  created_at: string;
+  updated_at: string;
+}
