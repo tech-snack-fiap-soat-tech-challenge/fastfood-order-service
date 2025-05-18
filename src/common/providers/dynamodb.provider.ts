15a978d787f456a76c1aa60385a3ebbefefec38a
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-05-18 17:12:59 -0300

    feat: enhance DynamoDB client configuration with improved logging and endpoint handling

--- src/common/providers/dynamodb.provider.ts
+++ src/common/providers/dynamodb.provider.ts
@@ -8,33 +8,23 @@ export const DYNAMO_CLIENT = 'DYNAMO_CLIENT';
 export const DynamoProvider: Provider = {
   provide: DYNAMO_CLIENT,
   useFactory: () => {
-    // Habilitar logs verbosos do SDK AWS
-    process.env.AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE = '1';
-    process.env.AWS_NODEJS_CONNECTION_REUSE_ENABLED = '1';
-
-    // Log dos parâmetros de configuração
-    console.log(
-      `Configurando DynamoDB na região: ${process.env.DYNAMODB_REGION}`,
-    );
-    console.log(`Access Key ID disponível: ${!!process.env.AWS_ACCESS_KEY_ID}`);
-    console.log(
-      `Secret Access Key disponível: ${!!process.env.AWS_SECRET_ACCESS_KEY}`,
-    );
-    console.log(`Session Token disponível: ${!!process.env.AWS_SESSION_TOKEN}`);
-
     try {
-      const config: DynamoDBClientConfig = {
-        region: process.env.DYNAMODB_REGION ?? 'us-east-1',
-        credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          sessionToken: process.env.AWS_SESSION_TOKEN,
-        },
-      };
+      let config: DynamoDBClientConfig;
 
       // Adiciona o endpoint apenas se a variável estiver definida
-      if (process.env.DYNAMODB_ENDPOINT) {
-        config.endpoint = process.env.DYNAMODB_ENDPOINT;
+      if (process.env.DYNAMO_ENDPOINT) {
+        config = {
+          endpoint: process.env.DYNAMO_ENDPOINT,
+        };
+      } else {
+        config = {
+          region: process.env.DYNAMODB_REGION ?? 'us-east-1',
+          credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: process.env.AWS_SESSION_TOKEN,
+          },
+        };
       }
 
       const client = new DynamoDBClient(config);
