5b7d741cbd81c7d62871b7fb25092a5191ba9002
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-05-18 19:20:11 -0300

    refactor: remove unused DynamoDB configuration interface and default export

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -63,17 +63,3 @@ export const pinoConfig = () => {
 
   return pinoOptions;
 };
-
-export interface DynamoConfig {
-  region: string;
-  endpoint: string;
-  accessKeyId: string;
-  secretAccessKey: string;
-}
-
-export default () => ({
-  region: process.env.DYNAMO_REGION,
-  endpoint: process.env.DYNAMO_ENDPOINT,
-  accessKeyId: process.env.DYNAMO_ACCESS_KEY_ID,
-  secretAccessKey: process.env.DYNAMO_SECRET_ACCESS_KEY,
-});
