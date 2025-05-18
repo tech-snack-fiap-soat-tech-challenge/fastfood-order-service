3f5b25fecf518e4911d18f0b2f646658f5cfc06a
Alexandre Maeda <ale.b.maeda@gmail.com>
2025-05-18 14:31:36 -0300

    feat: refactor DynamoDB client initialization to use configuration object

--- src/common/providers/dynamodb.provider.ts
+++ src/common/providers/dynamodb.provider.ts
@@ -1,5 +1,5 @@
 // src/common/providers/dynamodb.provider.ts
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { Provider } from '@nestjs/common';
 
@@ -8,10 +8,21 @@ export const DYNAMO_CLIENT = 'DYNAMO_CLIENT';
 export const DynamoProvider: Provider = {
   provide: DYNAMO_CLIENT,
   useFactory: () => {
-    const client = new DynamoDBClient({
+    const config: DynamoDBClientConfig = {
       region: process.env.DYNAMODB_REGION ?? 'us-east-1',
-      // endpoint: process.env.DYNAMODB_ENDPOINT ?? 'http://localhost:8000',
-    });
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        sessionToken: process.env.AWS_SESSION_TOKEN,
+      },
+    };
+
+    // Adiciona o endpoint apenas se a variável estiver definida
+    if (process.env.DYNAMODB_ENDPOINT) {
+      config.endpoint = process.env.DYNAMODB_ENDPOINT;
+    }
+
+    const client = new DynamoDBClient(config);
     return DynamoDBDocumentClient.from(client, {
       marshallOptions: {
         removeUndefinedValues: true, // Remove valores undefined
